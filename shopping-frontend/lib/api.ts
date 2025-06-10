bff8f4d3a32187089410ae53b947e27a03150b7b
Ali Raza <alirazakhalid1129@gmail.com>
2025-06-10 15:45:08 +0500

    get backend url from .env.local

--- shopping-frontend/lib/api.ts
+++ shopping-frontend/lib/api.ts
@@ -143,7 +143,8 @@ export interface BuyerDashboardData {
 }
 
 export class ApiService {
-  private static API_BASE_URL = "http://127.0.0.1:8000/api";
+  private static API_BASE_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}`;
+  // private static API_BASE_URL = "http://127.0.0.1:8000/api";
 
   private static async request<T>(
     endpoint: string,
--- shopping-frontend/lib/auth.ts
+++ shopping-frontend/lib/auth.ts
@@ -44,7 +44,7 @@ export interface User {
   updated_at?: string;
 }
 
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}`;
 
 export class AuthService {
   private static USER_KEY = "user";

5994c7cb1738236abbc8c779ac8c5ba5b03ca0e9
Ali Raza <129389379+Ali1raz@users.noreply.github.com>
2025-06-20 17:20:36 +0500

    Update ProductPermissionSeeder.php

--- laravel_backend/database/seeders/ProductPermissionSeeder.php
+++ laravel_backend/database/seeders/ProductPermissionSeeder.php
@@ -9,18 +9,6 @@ class ProductPermissionSeeder extends Seeder
 {
     public function run(): void
     {
-        // Product permissions
-        Permission::create(['name' => 'view products']);
-        Permission::create(['name' => 'create products']);
-        Permission::create(['name' => 'edit products']);
-        Permission::create(['name' => 'delete products']);
-
-        // Cart permissions
-        Permission::create(['name' => 'view cart']);
-        Permission::create(['name' => 'add to cart']);
-        Permission::create(['name' => 'remove from cart']);
-
-        // Bill permissions
-        Permission::create(['name' => 'view bills']);
+        // ...
     }
 }
